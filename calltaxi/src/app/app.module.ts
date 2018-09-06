c1839341625e76eb36ff0d11fead8ba4634731d5
muthumk90 <muthumk90@gmail.com>
2018-09-06 12:33:11 +0530

    Router has been added - router module has been added app module - created header, footer & container components - container used to display dynamic content - router-outlet is used to the container component

--- calltaxi/src/app/app.module.ts
+++ calltaxi/src/app/app.module.ts
@@ -1,14 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HeaderComponent } from './common/header/header.component';
+import { NavComponent } from './common/nav/nav.component';
+import { LoginComponent } from './container/login/login.component';
+import { RegisterComponent } from './container/register/register.component';
+import { MapComponent } from './container/map/map.component';
+import { AboutComponent } from './common/about/about.component';
+
+
+const appRoute:Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'map',
+    component:MapComponent
+  },
+  {
+    path:'',
+    component:MapComponent
+  },
+  {
+    path:'**',
+    redirectTo: '/map'
+  }
+
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent,
+    MapComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
